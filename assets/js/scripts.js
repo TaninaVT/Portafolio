6ce40b64e85251ae09264ac380c459fb05af1df0
Tania <taninavt@gmail.com>
2020-05-21 12:35:56 -0300

    Cuarto commit ajuste fonts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,24 @@
 $(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-  $('[data-toggle="popover"]').popover();
+  $("a").click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var guardaHash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(guardaHash).offset().top;
+      }, 800, function(){
+        window.location.hash = guardaHash;
+      });
+    }
+  });
+  // $('#example').tooltip(options);
+  $('#example').tooltip({ boundary: 'window' })
+
+  $('.example-popover').popover({
+      container: 'body'
+    });
+  //
+  // $('[data-toggle="tooltip"]').tooltip();
+  // $('[data-toggle="popover"]').popover();
 });
